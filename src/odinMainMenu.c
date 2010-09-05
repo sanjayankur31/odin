7658473570de6c28f18b1ba5ba7932ba855a86f7
Ankur Sinha <ankursinha@fedoraproject.org>
2010-09-06 00:40:30 +0530

    * Mon Sep 6 2010 Ankur Sinha <sanjay DOT ankur AT gmail DOT com> - Main Menu Complete:
    The rudimentary main menu is complete. I may need to add more options as the interface progresses.

--- src/odinMainMenu.c
+++ src/odinMainMenu.c
@@ -40,29 +40,47 @@
  * ===  FUNCTION  ======================================================================
  *         Name:  odinMainMenuDraw
  *  Description:  This method does the actual drawing. It is needed to implement the 
- *                  highlighting
+ *                highlighting.
  *
  *                  - Args:
- *                      char *options[] : options array
- *                      WINDOW *odinMainMenuWindow : the window to draw on
- *                      int odinCurrentHighlight : the option currently highlighted
- *                      int 
+ *                      char *options[] : options array.
+ *                      WINDOW *odinMainMenuWindow : the window to draw on.
+ *                      int odinMainMenuCols : number of columns in new window.
+ *                      int odinCurrentHighlight : the option currently highlighted.
+ *                      int odinStartRow : Begin printing from this row.
  *
  *                  - Returns: int
- *                      0 if all OK
- *                      non zero otherwise
+ *                      0 if all OK.
+ *                      non zero otherwise.
  *
  * =====================================================================================
  */
-    void
-odinMainMenuDraw (char *options[], WINDOW *odinMainMenuWindow, int odinCurrentHighlight )
+void
+odinMainMenuDraw (char *options[], WINDOW *odinMainMenuWindow,int odinMainMenuCols, int odinCurrentHighlight ,int odinStartRow)
 {
-    mvwprintw(odinMainMenuWindow,6,(odinMainMenuCols - strlen(odinMainMenuOptions[2]))/2, "%s",odinMainMenuOptions[2]);
-    mvwprintw(odinMainMenuWindow,7,(odinMainMenuCols - strlen(odinMainMenuOptions[3]))/2, "%s",odinMainMenuOptions[3]);
-    mvwprintw(odinMainMenuWindow,8,(odinMainMenuCols - strlen(odinMainMenuOptions[4]))/2, "%s",odinMainMenuOptions[4]);
-    mvwprintw(odinMainMenuWindow,9,(odinMainMenuCols - strlen(odinMainMenuOptions[5]))/2, "%s",odinMainMenuOptions[5]);
+    /*  Declarations  */
+    int odinCurrentRow = 0;
+    char **odinOptionsPtr;
+    char *odinTextPtr;
 
-    return 0;
+    odinOptionsPtr = options;
+    while(*odinOptionsPtr)
+    {
+        /*  Make the current one BOLD */
+        if(odinCurrentRow == odinCurrentHighlight)
+            wattron(odinMainMenuWindow, A_STANDOUT);
+
+        odinTextPtr = options[odinCurrentRow];
+        mvwprintw(odinMainMenuWindow,odinCurrentRow + odinStartRow,(odinMainMenuCols - strlen(odinTextPtr))/2, "%s",odinTextPtr);
+
+        if(odinCurrentRow == odinCurrentHighlight)
+            wattroff(odinMainMenuWindow, A_STANDOUT);
+        
+        odinCurrentRow++;
+        odinOptionsPtr++;
+    }
+
+    wrefresh(odinMainMenuWindow);
 }		/* -----  end of function odinMainMenuDraw  ----- */
 
 /* 
@@ -93,27 +111,103 @@ odinMainMenu ()
      * Options ?
      * Quit
      */
-    const char *odinMainMenuGreet[] = {"Main Menu", "=============" };
-    const char *odinMainMenuOptions[] = {"1.New Game", "2.Controls\\Documentation", "3.Options", "4.Quit" };
+    char *odinMainMenuGreet[] = {"Main Menu", "=============" };
+    char *odinMainMenuOptions[] = {"1.New Game", "2.Controls\\Documentation", "3.Options", "4.Quit", 0 };
 
+    /*  only needed for counting max number of options  */
+    char **odinOptions; 
+
+    /*  A new window for this  */
     WINDOW *odinMainMenuWindow;
     const int odinMainMenuCols  = 40;
     const int odinMainMenuLines = 20;
 
     /*  mimic high lighting 
-     *  range from 2 to 5 (indexes)*/
-    static int odinMainMenuOptionSelected = 2;
+     *  range from 0 to 3 (indexes)*/
+    static int odinOptionSelected = 0;
+
+    /* Variables needed for the highlighting */
+    int odinMaxRow = 0;
+    int odinSelected;
+    int odinKey = 0;
+    int odinStartRow = 6;
+
+    odinOptions = odinMainMenuOptions;
 
     odinMainMenuWindow = newwin(odinMainMenuLines,odinMainMenuCols,(LINES - odinMainMenuLines)/2,(COLS - odinMainMenuCols)/2);
-
     box(odinMainMenuWindow,0,0);
-    mvwprintw(odinMainMenuWindow,3,(odinMainMenuCols - strlen(odinMainMenuOptions[0]))/2, "%s",odinMainMenuGreet[0]);
-    mvwprintw(odinMainMenuWindow,4,(odinMainMenuCols - strlen(odinMainMenuOptions[1]))/2, "%s",odinMainMenuGreeet[1]);
 
+    mvwprintw(odinMainMenuWindow,3,(odinMainMenuCols - strlen(odinMainMenuGreet[0]))/2, "%s",odinMainMenuGreet[0]);
+    mvwprintw(odinMainMenuWindow,4,(odinMainMenuCols - strlen(odinMainMenuGreet[1]))/2, "%s",odinMainMenuGreet[1]);
 
     wrefresh(odinMainMenuWindow);
-    sleep(2);
 
-    return 0;
+    /*  count the total number of options */
+    while(*odinOptions)
+    {
+        odinMaxRow++;
+        odinOptions++;
+    }
+
+    /*  I don't think this is needed  */
+    if(odinOptionSelected >= odinMaxRow)
+    {
+        odinOptionSelected = 0;
+    }
+
+    keypad(odinMainMenuWindow, TRUE);
+    cbreak();
+    noecho();
+    odinKey = 0;
+
+    while(odinKey != 'q' && odinKey != KEY_ENTER && odinKey != '\n')
+    {
+        if(odinKey == KEY_UP)
+        {
+            /* if I'm on the first one, I need to come to the last one, like a circular queue */
+            if(odinOptionSelected == 0)
+            {
+                odinOptionSelected = odinMaxRow -1;
+            }
+            /*  else just subtract one */
+            else
+            {
+                odinOptionSelected--;
+            }
+        }
+
+
+        if(odinKey == KEY_DOWN)
+        {
+            /* If I'm on the last one, I need to come to the first one, like a circular queue */
+            if(odinOptionSelected == (odinMaxRow -1))
+            {
+                odinOptionSelected = 0;
+            }
+            /*  else just go to the next one  */
+            else 
+            {
+                odinOptionSelected++;
+            }
+        }
+
+        /*  for the functions return value, I'll probably revamp this  */
+        odinSelected = *odinMainMenuOptions[odinOptionSelected];
+
+        /*  Draw with new high light  */
+        odinMainMenuDraw(odinMainMenuOptions, odinMainMenuWindow, odinMainMenuCols, odinOptionSelected, odinStartRow );
+
+        /*  Get keypress, remember, we use wgetch because we're not in stdscr anymore  */
+        odinKey = wgetch(odinMainMenuWindow);
+    }
+    keypad(odinMainMenuWindow,FALSE);
+    nocbreak();
+    echo();
+
+    if(odinKey == 'q')
+        odinSelected = 'q';
+
+    return(odinSelected);
+
 }		/* -----  end of function odinMainMenu  ----- */
 

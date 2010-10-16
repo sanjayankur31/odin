667df474822f8c0a322a61df04e284e81bb4ece4
Ankur Sinha <ankursinha@fedoraproject.org>
2010-10-16 10:46:57 +0530

    * minor changes *	modified:   TODO - Minor additions
    *	new file:   src/odinResizeHandle.c
    *	new file:   src/odinResizeHandle.h
    - code for the handler for window resizes.
    TO BE WRITTEN

---
+++ src/odinResizeHandle.c
@@ -0,0 +1,47 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  odinResizeHandle.c
+ *
+ *    Description:  method that is called on a SIGWINCH
+ *
+ *        Version:  1.0
+ *        Created:  16/10/10 10:07:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ankur Sinha (FranciscoD), sanjay DOT ankur AT gmail DOT com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+/* Copyright 2010 Ankur Sinha 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  odinResizeHandle
+ *  Description:  
+ * =====================================================================================
+ */
+gint
+odinResizeHandle ( <+argument list+> )
+{
+    return <+return value+>;
+}		/* -----  end of function odinResizeHandle  ----- */

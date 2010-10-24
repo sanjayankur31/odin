be74c311be0aa37f4c73d0cf08edf2476a843a1f
Ankur Sinha <ankursinha@fedoraproject.org>
2010-10-24 11:31:52 +0530

    Begun working on MiniMax implementation. Will continue tomorrow in the laboratory

---
+++ src/odinMiniMax.c
@@ -0,0 +1,50 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  odinMiniMax.c
+ *
+ *    Description:  Implementation of the minimax algorithm for the AI
+ *
+ *        Version:  1.0
+ *        Created:  23/10/10 14:09:54
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ankur Sinha (FranciscoD), sanjay DOT ankur AT gmail DOT com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+/* Copyright 2010 Ankur Sinha 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include	"odinMiniMax.h"
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  odinMiniMax
+ *  Description:  
+ * =====================================================================================
+ */
+gint
+odinMiniMax ( <+argument list+> )
+{
+    return <+return value+>;
+}		/* -----  end of function odinMiniMax  ----- */
